3d60f88892679ff7262bee20368db37c6f525ddf
WhyTellMeWhy <swhangenious@naver.com>
2025-01-08 23:36:40 +0900

    1차 완성
    남은것: 모바일 메뉴, https설정

--- main.py
+++ main.py
@@ -10,4 +10,32 @@ def home():
 def ceo():
     return render_template("info/ceo.html")
 
+@app.route('/info/vision')
+def vision():
+    return render_template("info/vision.html")
+
+@app.route('/info/organization')
+def organization():
+    return render_template("info/organization.html")
+
+@app.route('/buis/Attendify')
+def Attendify():
+    return render_template("buis/Attendify.html")
+
+@app.route('/promotions')
+def promotions():
+    return '<script>alert("프로모션 준비중입니다.");history.back();</script>'
+
+@app.route('/customer/notice')
+def notice():
+    return render_template("customer/notice.html")
+
+@app.route('/customer/qna')
+def qna():
+    return render_template("customer/qna.html")
+
+@app.route('/customer/inquiry')
+def inquiry():
+    return render_template("customer/inquiry.html")
+
 app.run(port=80, debug=True)
